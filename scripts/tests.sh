2ad962a31602e63d6a842547b7e84e29804b6de5
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-07-04 11:44:39 -0300

    Adapto scripts para correr bajo docker

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -4,7 +4,7 @@ DIR=$(dirname "$0")
 cd ${DIR}/..
 
 echo "Running py.test"
-env PYTHONDONTWRITEBYTECODE=1 py.test -v $@
+docker exec -it infra_django_1 env PYTHONDONTWRITEBYTECODE=1 DJANGO_SETTINGS_MODULE=conf.settings.testing py.test -v -p no:cacheprovider $@
 echo "py.test OK :)"
 
 
