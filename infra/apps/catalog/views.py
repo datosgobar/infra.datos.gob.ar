dc7da97f99ce655a74fa95a8bb6992d156de59ef
Franco Scaccheri Cassanello <fcassanello@devartis.com>
2019-07-11 15:35:04 -0300

    Uso de temp files para subida de archivos

--- infra/apps/catalog/views.py
+++ infra/apps/catalog/views.py
@@ -1,19 +1,19 @@
 import errno
-import json
 import os
+import uuid
 from urllib import request as urllib_request
 from urllib.error import HTTPError
 
+from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.core.files import File
-from django.core.files.storage import FileSystemStorage
+from django.core.files.temp import NamedTemporaryFile
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
 from pydatajson import DataJson
 
 from infra.apps.catalog.forms import CatalogForm
 from infra.apps.catalog.models import Catalog
-
-from django.conf import settings
 
 
 class CatalogView(ListView):
@@ -32,14 +32,17 @@ class AddCatalogView(FormView):
             return self.form_valid(form)
         return self.render_to_response(self.get_context_data(form=form))
 
-    def form_valid(self, form):
+    def generate_form_data(self, form):
         file_format = form.cleaned_data['format']
-        # TODO: Crear tmp_file para manipular tanto en el caso de archivo o URL, y pasar ese tmp_file
-        #  como parametro de DataJson
 
         if form.cleaned_data['file']:
+            temp_file = NamedTemporaryFile(dir='/tmp', prefix=str(uuid.uuid4()))
             file_to_upload = form.cleaned_data['file']
-            identifier = json.loads(file_to_upload.read())['identifier']
+            temp_file.write(file_to_upload.read())
+            identifier = DataJson(temp_file.name).get('identifier')
+            temp_file.close()
+            if not identifier:
+                raise ValidationError('El catálogo subido no posee el campo "identifier"')
         else:
             # Download the file contents from the specified URL
             try:
@@ -47,17 +50,16 @@ class AddCatalogView(FormView):
             except HTTPError as e:
                 raise ConnectionError(f'No se pudo conectar a la URL ingresada: {e}')
 
-
-            # TODO: validar que el response sea 200, no sea còdigo HTML, y que haya identifier
             identifier = DataJson(form.cleaned_data['url'])['identifier']
-
+            if not identifier:
+                raise ValidationError('El catálogo indicado no posee el campo "identifier"')
 
             path = f'{settings.MEDIA_ROOT}/catalogs/{identifier}/data.{file_format}'
 
             if not os.path.exists(os.path.dirname(path)):
                 try:
                     os.makedirs(os.path.dirname(path))
-                except OSError as exc:  # Guard against race condition
+                except OSError as exc:
                     if exc.errno != errno.EEXIST:
                         raise OSError("Hubo un problema con el guardado del archivo del catálogo.")
 
@@ -65,9 +67,15 @@ class AddCatalogView(FormView):
             final_file.write(file_content)
             file_to_upload = File(file=final_file)
 
+        return identifier, file_format, file_to_upload
+
+    def form_valid(self, form):
+
+        identifier, file_format, file_to_upload = self.generate_form_data(form)
+
         Catalog.objects.create(
             identifier=identifier,
-            format=form.cleaned_data['format'],
+            format=file_format,
             file=file_to_upload
         )
 
