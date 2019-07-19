6f8755db68bf09228575ed7b64b57756097c28f9
fcassanello <fcassanello@gitlab.com>
2019-07-19 15:09:00 -0300

    Validacion de archivos o URL en formato JSON agregada

--- infra/apps/catalog/views.py
+++ infra/apps/catalog/views.py
@@ -4,6 +4,9 @@ from django.core.exceptions import ValidationError
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
+from pydatajson import DataJson
+from pydatajson.readers import read_catalog
+from django.conf import settings
 
 from infra.apps.catalog.forms import CatalogForm
 from infra.apps.catalog.models import Catalog
@@ -25,9 +28,23 @@ class AddCatalogView(FormView):
             return self.form_invalid(form)
 
         try:
-            Catalog.create_from_url_or_file(form.cleaned_data)
+            catalog = Catalog.create_from_url_or_file(form.cleaned_data)
         except ValidationError as e:
             messages.error(request, e)
+            return self.form_invalid(form)
+
+        catalog_file_path = 'media/' + catalog.file.name
+        catalog_content = read_catalog(catalog_file_path)
+
+        dj = DataJson()
+
+        if not dj.is_valid_catalog(catalog_content):
+            catalog.delete()
+            error_report = dj.validate_catalog(catalog_content)
+            errors = error_report['error']['catalog']['errors']
+            error_messages = '. '.join([error['message'] for error in errors])
+            messages.error(request, "El catálogo ingresado no es valido: " + error_messages)
+
             return self.form_invalid(form)
 
         return self.form_valid(form)
