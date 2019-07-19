7183b8613cafab6dcc6a0dced21f7e951e26db17
fcassanello <fcassanello@gitlab.com>
2019-07-19 17:43:25 -0300

    Refactorizo el codigo de validacion de catalogos en la vista

--- infra/apps/catalog/views.py
+++ infra/apps/catalog/views.py
@@ -1,12 +1,11 @@
 # coding=utf-8
+from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ValidationError
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
 from pydatajson import DataJson
-from pydatajson.readers import read_catalog
-from django.conf import settings
 
 from infra.apps.catalog.forms import CatalogForm
 from infra.apps.catalog.models import Catalog
@@ -33,30 +32,33 @@ class AddCatalogView(FormView):
             messages.error(request, e)
             return self.form_invalid(form)
 
-        catalog_file_path = 'media/' + catalog.file.name
+        return self.validate_catalog(request, form, catalog)
+
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        response.status_code = 400
+        return response
+
+    def validate_catalog(self, request, form, catalog):
+        catalog_file_path = settings.MEDIA_ROOT + '/' + catalog.file.name
+
         try:
             dj = DataJson(catalog_file_path)
         except KeyError:
             catalog.delete()
             messages.error(request, "No se puede validar el catálogo ingresado")
             return self.form_invalid(form)
-        else:
-            if not dj.is_valid_catalog():
-                catalog.delete()
-                error_report = dj.validate_catalog()
-                errors = error_report['error']['catalog']['errors']
 
-                for dataset in error_report['error']['dataset']:
-                    errors += dataset['errors']
+        if not dj.is_valid_catalog():
+            catalog.delete()
+            error_report = dj.validate_catalog()
+            errors = error_report['error']['catalog']['errors']
 
-                error_messages = '. '.join([error['message'] for error in errors])
-                messages.error(request, "El catálogo ingresado no es valido: " + error_messages)
+            for dataset in error_report['error']['dataset']:
+                errors += dataset['errors']
 
-                return self.form_invalid(form)
+            error_messages = '. '.join([error['message'] for error in errors])
+            messages.error(request, "El catálogo ingresado no es valido: " + error_messages)
+            return self.form_invalid(form)
 
         return self.form_valid(form)
-
-    def form_invalid(self, form):
-        response = super().form_invalid(form)
-        response.status_code = 400
-        return response
