afd71547a74925de055409a5a76f8a53166c0aa9
Christian Digiorno <cdigiorno@devartis.com>
2019-07-23 12:12:18 -0300

    Creo vistas para el listado de nodos y para buscar los catálogos de uno

--- infra/apps/catalog/urls.py
+++ infra/apps/catalog/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from infra.apps.catalog import views as catalog_views
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', catalog_views.CatalogView.as_view(), name='list'),
-    path('add/', catalog_views.AddCatalogView.as_view(), name='add')
+    path('add/', catalog_views.AddCatalogView.as_view(), name='add'),
+    path('nodes/', catalog_views.NodeListView.as_view(), name='nodes'),
+    path('nodes/<int:id>/', catalog_views.NodeUploadsView.as_view(), name='node_catalogs')
 ]
--- infra/apps/catalog/views.py
+++ infra/apps/catalog/views.py
@@ -1,12 +1,13 @@
 # coding=utf-8
 from django.contrib import messages
 from django.core.exceptions import ValidationError
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import FormView
 
 from infra.apps.catalog.forms import CatalogForm
-from infra.apps.catalog.models import CatalogUpload
+from infra.apps.catalog.models import *
 
 
 class CatalogView(ListView):
@@ -36,3 +37,19 @@ class AddCatalogView(FormView):
         response = super().form_invalid(form)
         response.status_code = 400
         return response
+
+
+class NodeListView(ListView):
+    model = Node
+    template_name = "nodes/index.html"
+
+
+class NodeUploadsView(ListView):
+    model = CatalogUpload
+    template_name = "nodes/uploads.html"
+
+    def get(self, request, *args, **kwargs):
+        node_id = self.kwargs['id']
+        node_name = Node.objects.get(pk=node_id).identifier
+        node_uploads = self.model.objects.filter(node=node_id)
+        return render(request, self.template_name, {'object_list': node_uploads, 'node_name': node_name})
