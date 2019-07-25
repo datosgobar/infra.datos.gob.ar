cc6db3d88d861435f84a24b45a632a8010d1b230
Lucas Romero <lromero@devartis.com>
2019-07-25 18:40:43 -0300

    Renombro el kwarg de las urls por node_id

--- infra/apps/catalog/urls.py
+++ infra/apps/catalog/urls.py
@@ -7,11 +7,9 @@ app_name = 'catalog'
 urlpatterns = [
     path('', catalog_views.CatalogView.as_view(), name='list'),
     path('add/', catalog_views.AddCatalogView.as_view(), name='add'),
-    path('add/<int:node>/', catalog_views.AddDistribution.as_view(), name='add_distribution'),
-    path('<int:node>/distributions/',
-         catalog_views.ListDistributions.as_view(),
-         name='list_distributions'),
+    path('add/<int:node_id>/', catalog_views.AddDistribution.as_view(), name='add_distribution'),
+    path('<int:node_id>/distributions/', catalog_views.ListDistributions.as_view(), name='list_distributions'),
     path('nodes/', catalog_views.NodeListView.as_view(), name='nodes'),
-    path('nodes/<int:id>/', catalog_views.NodeUploadsView.as_view(), name='node_catalogs'),
+    path('nodes/<int:node_id>/', catalog_views.NodeUploadsView.as_view(), name='node_catalogs'),
     path('success/', catalog_views.CatalogUploadSuccess.as_view(), name='upload_success')
 ]
