803025365e64c6ae54163ee701c370ae38d9df6f
Lucas Romero <lromero@devartis.com>
2019-07-25 18:37:31 -0300

    Agrego admins a los nodos

--- infra/apps/catalog/models/node.py
+++ infra/apps/catalog/models/node.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 from infra.apps.catalog.exceptions.catalog_not_uploaded_error import CatalogNotUploadedError
+from infra.apps.users.models import User
 
 
 class Node(models.Model):
     identifier = models.CharField(max_length=20, unique=True)
+    admins = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.identifier
