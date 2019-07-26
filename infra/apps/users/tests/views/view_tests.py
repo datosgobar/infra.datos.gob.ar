8e97b5ec872623d1a814630b1a1b5aee5d5a6861
Christian Digiorno <cdigiorno@devartis.com>
2019-07-26 11:09:11 -0300

    Agrego test para logout

--- infra/apps/users/tests/views/view_tests.py
+++ infra/apps/users/tests/views/view_tests.py
@@ -1,6 +1,6 @@
 import pytest
 from django.test import Client
-from django.urls import resolve
+from django.urls import resolve, reverse
 
 CLIENT = Client()
 pytestmark = pytest.mark.django_db
@@ -21,3 +21,10 @@ def test_username_shows_if_user_is_logged_in(user):
     CLIENT.force_login(user)
     response = CLIENT.get(CLIENT.get('/').url)
     assert user.username in response.content.decode('utf-8')
+
+
+def test_logout_correctly_logs_user_out(user):
+    CLIENT.force_login(user)
+    CLIENT.get(reverse('logout'))
+    response = CLIENT.get('/')
+    assert response.status_code == 302 and resolve(response.url.split("?")[0]).url_name == 'login'
