775d8c44aaeb279bc4407140f31ba280e50a5f7a
Lucas Lavandeira <lucaslavandeira@gmail.com>
2019-08-12 10:04:09 -0300

    Agrego botón para volver atrás en seleccion de file o URL

--- infra/apps/catalog/static/file_or_url.js
+++ infra/apps/catalog/static/file_or_url.js
@@ -1,5 +1,6 @@
 window.onload = function () {
-    fileOrUrl();
+    showFieldEvents();
+    returnEvents();
 };
 
 
@@ -7,7 +8,11 @@ function hideHelp() {
     document.getElementById("file-or-url").className += " hidden";
 }
 
-function fileOrUrl() {
+function showHelp() {
+    document.getElementById("file-or-url").classList.remove("hidden");
+}
+
+function showFieldEvents() {
     var file_button = document.getElementById("file-button");
     file_button.onclick = function () {
         document.getElementById("file-field").classList.remove("hidden");
@@ -17,5 +22,20 @@ function fileOrUrl() {
     url_button.onclick = function () {
         document.getElementById("url-field").classList.remove("hidden");
         hideHelp();
+    };
+}
+
+function returnEvents() {
+    var fileButton = document.getElementById("file-return-button");
+    fileButton.onclick = function () {
+        document.getElementById("file-field").classList.add("hidden");
+        document.getElementById("id_file").value = "";
+        showHelp();
+    };
+    var url_button = document.getElementById("url-return-button");
+    url_button.onclick = function () {
+        document.getElementById("url-field").classList.add("hidden");
+        document.getElementById("id_url").value = "";
+        showHelp();
     };
 }
