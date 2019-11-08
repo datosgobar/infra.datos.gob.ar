ad9b994de9521c5bc49941563b0edb8ea139fcac
Lucas Lavandeira <19612265+lucaslavandeira@users.noreply.github.com>
2019-11-08 17:17:12 -0300

    Borro comment duplicado

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 # Do not use a dir inside the project in production environments
 MEDIA_ROOT = BASE_DIR('media')
 MEDIA_URL = '/media/'
