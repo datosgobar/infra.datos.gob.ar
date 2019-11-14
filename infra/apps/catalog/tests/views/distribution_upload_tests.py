fc12200503f21cf2403dde575da7f7bb17459ce2
Alexis Wolfsdorf <awolfsdorf@devartis.com>
2019-11-14 12:39:41 -0300

    correccion de test, no pedian que el usuario este loggeado ni con permisos de administrador del nodo para ver sus distribuciones

--- infra/apps/catalog/tests/views/distribution_upload_tests.py
+++ infra/apps/catalog/tests/views/distribution_upload_tests.py
@@ -8,22 +8,27 @@ from infra.apps.catalog.tests.helpers.open_catalog import open_catalog
 pytestmark = pytest.mark.django_db
 
 
+@pytest.fixture(autouse=True)
+def give_user_edit_rights(user, node):
+    node.admins.add(user)
+
+
 def _call(client, distribution):
     return client.get(reverse('catalog:distribution_uploads',
                               kwargs={'node_id': distribution.catalog.id,
                                       'identifier': distribution.identifier}))
 
 
-def test_older_versions_listed(client, distribution_upload):
+def test_older_versions_listed(logged_client, distribution_upload):
     distribution = distribution_upload.distribution
     with freeze_time('2019-01-01'):
         with open_catalog('test_data.csv') as fd:
             other = distribution.distributionupload_set \
                 .create(file=File(fd))
-    response = _call(client, distribution)
+    response = _call(logged_client, distribution)
     assert str(other.uploaded_at) in response.content.decode('utf-8')
 
 
-def test_catalog_identifier_in_page(client, distribution):
-    response = _call(client, distribution)
+def test_catalog_identifier_in_page(logged_client, distribution):
+    response = _call(logged_client, distribution)
     assert distribution.catalog.identifier in response.content.decode('utf-8')
