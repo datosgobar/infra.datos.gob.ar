4f55adf3b2d6e5116ec6932f28136b06bf453013
fcassanello <fcassanello@gitlab.com>
2019-11-25 10:43:05 -0300

    Agrego tests de la vista de lista de distribuciones de un nodo

--- infra/apps/catalog/tests/views/node_distributions_tests.py
+++ infra/apps/catalog/tests/views/node_distributions_tests.py
@@ -9,11 +9,22 @@ def give_user_edit_rights(user, node):
     node.admins.add(user)
 
 
-def _call(client, distribution):
-    return client.get(reverse('catalog:node_distributions',
-                              kwargs={'node_id': distribution.catalog.id}))
+def _call(client, distribution_upload, selected_dataset=None):
+    url = reverse('catalog:node_distributions',
+                  kwargs={'node_id': distribution_upload.distribution.catalog.id})
+    if selected_dataset:
+        url += '?dataset_identifier={}'.format(selected_dataset)
+    return client.get(url)
 
 
-def test_dataset_identifiers_in_page(logged_client, distribution):
-    response = _call(logged_client, distribution)
-    assert distribution.catalog.identifier in response.content.decode('utf-8')
+def test_dataset_identifiers_in_context(logged_client, distribution_upload):
+    response = _call(logged_client, distribution_upload)
+    dataset_identifiers = [dataset[0] for dataset in response.context['dataset_list']]
+    assert distribution_upload.distribution.dataset_identifier in dataset_identifiers
+
+
+def url_dataset_is_selected_dataset(logged_client, distribution_upload):
+    distribution_dataset_id = distribution_upload.distribution.dataset_identifier
+    response = _call(logged_client, distribution_upload,
+                     selected_dataset=distribution_upload.distribution.dataset_identifier)
+    assert distribution_dataset_id == response.context['selected_dataset']
